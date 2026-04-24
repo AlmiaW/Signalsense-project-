9a9ef1299470bb466d8e75b2de9dfb94dfd3f917
Almia williams <awill994@students.kennesaw.edu>
2026-04-24 13:57:51 -0400

    Added thank you message after response time input

--- Main.py
+++ Main.py
@@ -6,3 +6,5 @@ print("Welcome to SignalSense")
 
 response_time = input("Enter response time in minutes: ")
 print("Response time recorded:", response_time)
+
+print("Thank you for using SignalSense")
